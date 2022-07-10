6b2f6ba4da8dc4568634024ee6db856a797b8778
Misdar <50019528+misdarmanto@users.noreply.github.com>
2022-07-10 15:19:46 +0700

    Update firebase.js

--- helpers/firebase.js
+++ helpers/firebase.js
@@ -3,13 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCYYzCSTopYc3JerPxnB17PIfKjtc50c5w",
-  authDomain: "utbk-747d7.firebaseapp.com",
-  projectId: "utbk-747d7",
-  storageBucket: "utbk-747d7.appspot.com",
-  messagingSenderId: "239387520436",
-  appId: "1:239387520436:web:c461cdc9004cbedc8f16e1",
-  measurementId: "G-XZ7LRPN79Z",
+
 };
 
 const app = initializeApp(firebaseConfig);
